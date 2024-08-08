89593d847cee834ab702926a9b85dfdafdb8fa60
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-08-08 09:38:22 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/CursoCancinoSecurityApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoCancinoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoCancinoSecurityApplication.class, args);
+	}
+
+}
