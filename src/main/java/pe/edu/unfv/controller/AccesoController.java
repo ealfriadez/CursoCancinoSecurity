4261322251c9cc92d965cd928444d2a011235a3d
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-08-10 11:53:54 -0500

    No logro identificar la falla

--- src/main/java/pe/edu/unfv/controller/AccesoController.java
+++ src/main/java/pe/edu/unfv/controller/AccesoController.java
@@ -12,29 +12,52 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/acceso")
 public class AccesoController {
 
+	/*
+	 * @GetMapping("/login") public String login_(
+	 * 
+	 * @RequestParam(value = "error", required = false) String error,
+	 * 
+	 * @RequestParam(value = "logout", required = false) String logout,
+	 * RedirectAttributes flash, Principal principal){
+	 * 
+	 * if (principal != null) { flash.addFlashAttribute("clase", "success");
+	 * flash.addFlashAttribute("mensaje", "Ya ha iniciado sesion anteriormente");
+	 * return "redirect:/"; } if (error != null) { flash.addFlashAttribute("clase",
+	 * "danger"); flash.addFlashAttribute("mensaje",
+	 * "Los datos ingresados no son correctos, por favor vuelva a intentarlo.");
+	 * return "redirect:/acceso/login"; } if (logout != null) {
+	 * flash.addFlashAttribute("clase", "success");
+	 * flash.addFlashAttribute("mensaje", "Ha cerrado la sesion exitosamente.");
+	 * return "redirect:/acceso/login"; }
+	 * 
+	 * return "acceso/login"; }
+	 */
+	
 	@GetMapping("/login")
 	public String login(
-			@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout,
-			RedirectAttributes flash,
-			Principal principal){
-		
-		if (principal != null) {
+			@RequestParam(value = "error", required = false) String error, 
+			@RequestParam(value="logout", required=false) String logout , 
+			RedirectAttributes flash, 
+			Principal principal )
+	{
+		if(principal!=null) 
+		{
 			flash.addFlashAttribute("clase", "success");
-			flash.addFlashAttribute("mensaje", "Ya ha iniciado sesion anteriormente");
-			return "redirect:/";
+			flash.addFlashAttribute("mensaje", "Ya ha iniciado sesión anteriormente ");
+			return "redirect:/acceso/login";
 		}
-		if (error != null) {
+		if(error!=null) 
+		{
 			flash.addFlashAttribute("clase", "danger");
 			flash.addFlashAttribute("mensaje", "Los datos ingresados no son correctos, por favor vuelva a intentarlo.");
 			return "redirect:/acceso/login";
 		}
-		if (logout != null) {
+		if(logout!=null) 
+		{
 			flash.addFlashAttribute("clase", "success");
-			flash.addFlashAttribute("mensaje", "Ha cerrado la sesion exitosamente.");
+			flash.addFlashAttribute("mensaje", "Ha cerrado sesión exitosamente");
 			return "redirect:/acceso/login";
 		}
-		
 		return "acceso/login";
 	}
 }
