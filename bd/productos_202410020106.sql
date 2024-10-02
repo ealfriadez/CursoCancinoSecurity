7535bc984b3996f84ffd1748404815fd44e3731b
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-10-02 01:30:02 -0500

    Avanzando con los test

---
+++ bd/productos_202410020106.sql
@@ -0,0 +1,16 @@
+INSERT INTO cancino.productos (categoria_id,nombre,slug,descripcion,precio,foto,nombreFoto,tipoFoto,imageData) VALUES
+	 (1,'Producto 1','100100100','producto-1',100,'foto-1',NULL,NULL,NULL),
+	 (1,'Producto 1','100100100','producto-1',100,'foto-1',NULL,NULL,NULL),
+	 (1,'Producto 2','100100101','producto-2',200,'foto-2',NULL,NULL,NULL),
+	 (1,'Producto 3','100100102','producto-3',300,'foto-3',NULL,NULL,NULL),
+	 (2,'Producto 4','100100111','producto-4',400,'foto-4',NULL,NULL,NULL),
+	 (2,'Producto 5','100100122','producto-5',500,'foto-5',NULL,NULL,NULL),
+	 (2,'Producto 6','100100133','producto-6',600,'foto-6',NULL,NULL,NULL),
+	 (3,'Producto 7','100100200','producto-7',700,'foto-7',NULL,NULL,NULL),
+	 (3,'Producto 8','100100300','producto-8',800,'foto-8',NULL,NULL,NULL),
+	 (3,'Producto 9','100100400','producto-9',900,'foto-9',NULL,NULL,NULL);
+INSERT INTO cancino.productos (categoria_id,nombre,slug,descripcion,precio,foto,nombreFoto,tipoFoto,imageData) VALUES
+	 (3,'Producto 10','100100500','producto-10',1000,'foto-10',NULL,NULL,NULL),
+	 (2,'Tronsmart','tronsmart','Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',1500,'1722463117715.png',NULL,NULL,NULL),
+	 (1,'ok','kk','abc',15,'12345','oh.pdf','jj',0x6667687479727472747972743132333231);
